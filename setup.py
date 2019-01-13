4f3ed81c7e3673b11d561f37d12bdac8998fe1cd
mouradmourafiq <mouradmourafiq@gmail.com>
2019-01-13 17:51:19 +0100

    Bump version to 0.3.6

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ class PyTest(TestCommand):
 
 
 setup(name='polyaxon-k8s',
-      version='0.3.5',
+      version='0.3.6',
       description='Polyaxon Kubernetes managers, utils, and resources.',
       maintainer='Mourad Mourafiq',
       maintainer_email='mourad@polyaxon.com',
